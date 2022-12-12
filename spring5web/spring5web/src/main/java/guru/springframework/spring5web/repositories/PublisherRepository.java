f48aa9e1bf2eb526e4d7730469be80b4973abd3d
Marko Zupanic <104766005+markozupanic@users.noreply.github.com>
2022-12-12 20:42:14 +0100

    ¸fewf

--- spring5web/spring5web/src/main/java/guru/springframework/spring5web/repositories/PublisherRepository.java
+++
@@ -1,7 +0,0 @@
-package guru.springframework.spring5web.repositories;
-
-import guru.springframework.spring5web.domain.Publisher;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PublisherRepository extends CrudRepository<Publisher, Long> {
-}
