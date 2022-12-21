c8c5acfbc66a9c4439a7d1cecfc839316efe04f3
Marko Zupanic <104766005+markozupanic@users.noreply.github.com>
2022-12-21 14:19:35 +0100

    Qualifiers

--- sfg-di/src/test/java/com/example/sfgdi/controllers/PropertyInjctedControllerTest.java
+++ sfg-di/src/test/java/com/example/sfgdi/controllers/PropertyInjctedControllerTest.java
@@ -1,10 +1,8 @@
 package com.example.sfgdi.controllers;
 
-import com.example.sfgdi.services.GreetingServiceImpl;
+import com.example.sfgdi.services.ConstructorGreetingService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class PropertyInjctedControllerTest {
 
@@ -13,7 +11,7 @@ class PropertyInjctedControllerTest {
     @BeforeEach
     void setUp() {
         controller =new PropertyInjctedController();
-        controller.greetingService=new GreetingServiceImpl();
+        controller.greetingService=new ConstructorGreetingService();
     }
 
     @Test
--- sfg-di/src/test/java/com/example/sfgdi/controllers/SetterUnjectedControllerTest.java
+++ sfg-di/src/test/java/com/example/sfgdi/controllers/SetterUnjectedControllerTest.java
@@ -1,10 +1,8 @@
 package com.example.sfgdi.controllers;
 
-import com.example.sfgdi.services.GreetingServiceImpl;
+import com.example.sfgdi.services.ConstructorGreetingService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class SetterUnjectedControllerTest {
 
@@ -13,7 +11,7 @@ class SetterUnjectedControllerTest {
     @BeforeEach
     void setUp() {
         controller = new SetterUnjectedController();
-        controller.setGreetingService(new GreetingServiceImpl());
+        controller.setGreetingService(new ConstructorGreetingService());
     }
 
     @Test
