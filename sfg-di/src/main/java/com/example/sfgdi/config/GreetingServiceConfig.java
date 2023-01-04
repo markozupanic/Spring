da2a620c8deb9a5a026188c98300c5500d6c76f7
Marko Zupanic <104766005+markozupanic@users.noreply.github.com>
2023-01-04 13:40:23 +0100

    Factory beans

--- sfg-di/src/main/java/com/example/sfgdi/config/GreetingServiceConfig.java
+++ sfg-di/src/main/java/com/example/sfgdi/config/GreetingServiceConfig.java
@@ -3,6 +3,8 @@ package com.example.sfgdi.config;
 import com.example.sfgdi.repositories.EnglishGreeting;
 import com.example.sfgdi.repositories.EnglishGreetingImpl;
 import com.example.sfgdi.services.*;
+import com.springframework.pets.PetService;
+import com.springframework.pets.PetServiceFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -10,6 +12,23 @@ import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
+
+    @Bean
+    PetServiceFactory petServiceFactory(){
+        return new PetServiceFactory();
+    }
+
+    @Profile({"dog", "default"})
+    @Bean
+    PetService petService(PetServiceFactory petServiceFactory){
+        return petServiceFactory.getPetService("dog");
+    }
+
+    @Profile("cat")
+    @Bean
+    PetService catPetService(PetServiceFactory petServiceFactory){
+        return petServiceFactory.getPetService("cat");
+    }
 
     @Profile({"ES","default"})
     @Bean("i18nService")
